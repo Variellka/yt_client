294d56024186162ef9a1d0573b8e80adae3f6e81
Xenius <variellka@gmail.com>
2020-10-26 00:33:01 +0300

    add flip

--- src/Components/VideoInformation.jsx
+++ src/Components/VideoInformation.jsx
@@ -7,20 +7,34 @@ export const VideoInformation = ({video}) => {
     const videoAuthor = video.snippet.channelTitle
     const videoDescription = video.snippet.description
     const videoURL =  video.id.videoId
-
     const videoPublished = video.snippet.publishedAt
     const videoDateParse = new Date(Date.parse(videoPublished)).toString().slice(4, 15);
 
+    const handleClick = () => {
+        document.querySelector('.flip-container').classList.toggle('flip')
+    }
+
     return (
-        <div className={'video-info'}>
-            <img src={videoImage}/>
-            <p>{videoTitle}</p>
-            <p><strong>{videoAuthor}</strong></p>
-            <p><em>{videoDateParse}</em></p>
-            <button >Description</button>
-            <div className={'video-description block'}>
-                <p>{videoDescription}</p>
-                <a href={`https://www.youtube.com/watch?v=${videoURL}`}>Watch full video</a>
+        <div className={'flip-container'}>
+            <div className={'flipper'}>
+                <div className={'front'}>
+                    <div className={'image-and-title'}>
+                        <img src={videoImage}/>
+                        <div className={'video-title-block'}>
+                            <p className={'video-title'}>{videoTitle}</p>
+                        </div>
+                    </div>
+                    <div className={'video-info-block'}>
+                        <p className={'video-info'}>{videoAuthor}</p>
+                        <p className={'video-info'}>{videoDateParse}</p>
+                    </div>
+                    <button className={'video-button'} onClick={handleClick}>Description</button>
+                </div>
+                <div className={'back'}>
+                    <p>{videoDescription}</p>
+                    <a href={`https://www.youtube.com/watch?v=${videoURL}`} target={'blank'}>Watch full video</a>
+                    <button className={'video-button'}>Back</button>
+                </div>
             </div>
         </div>
     )
