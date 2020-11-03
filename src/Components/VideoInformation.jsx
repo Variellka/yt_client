47e72b4f8704b4f7e360f33cb4866ca42b36fb20
Xenius <variellka@gmail.com>
2020-11-03 23:07:14 +0300

    requests

--- src/Components/VideoInformation.jsx
+++ src/Components/VideoInformation.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import './Search.css'
 import person from '../Images/person.png'
 import calendar from '../Images/calendar.png'
+import { makeAFlip } from "../Requests/requests"
 
 export const VideoInformation = ({video}) => {
+
     const videoImage = video.snippet.thumbnails.high.url
     const videoTitle = video.snippet.title
     const videoAuthor = video.snippet.channelTitle
@@ -11,12 +13,6 @@ export const VideoInformation = ({video}) => {
     const videoURL =  video.id.videoId
     const videoPublished = video.snippet.publishedAt
     const videoDateParse = new Date(Date.parse(videoPublished)).toString().slice(4, 15);
-
-    const handleClick = (event) => {
-        const target = event.target;
-        const parent = target.parentElement
-        parent.parentElement.classList.toggle('flip')
-    }
 
     return (
         <div className={'flip-container'}>
@@ -36,14 +32,14 @@ export const VideoInformation = ({video}) => {
                             <img src={calendar} className={'person-img'}/><p className={'video-info'}>{videoDateParse}</p>
                         </div>
                     </div>
-                    <button className={'video-button'} onClick={handleClick}>Description</button>
+                    <button className={'video-button'} onClick={makeAFlip}>Description</button>
                 </div>
                 <div className={'back'}>
                     <div className={'video-info-back-block'}>
                         <p className={'video-description'}>{videoDescription}</p>
                         <a href={`https://www.youtube.com/watch?v=${videoURL}`} target={'blank'}>Watch full video</a>
                     </div>
-                    <button className={'video-button'} onClick={handleClick}>Back</button>
+                    <button className={'video-button'} onClick={makeAFlip}>Back</button>
                 </div>
             </div>
         </div>
